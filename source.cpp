b73259d6169af74a60152cec99f1e0c29730b96d
DELL <DELL@DESKTOP-5L4OD54>
2026-04-24 17:17:18 +0300

    Add project files.

---
+++ source.cpp
@@ -0,0 +1,182 @@
+#include <iostream>
+#include <conio.h>
+using namespace std;
+
+// Game Settings //
+int isJumping = 0;
+int jumpdone;
+int pos;
+int velocety = 3; //controls jump height
+
+void display(char x[][120])
+{
+	system("cls");
+	for (int r = 0; r < 25; r++)
+	{
+		for (int c = 0; c < 120; c++)
+		{
+			cout << x[r][c];
+		}
+		cout << endl;
+	}
+}
+
+void border(char x[][120])
+{
+	for (int r = 0; r < 25; r++)
+	{
+		for (int c = 0; c < 120; c++)
+		{
+			x[r][c] = ' ';
+		}
+	}
+	
+	for (int r = 0; r < 25; r++)
+	{
+
+		x[r][0] = 186;
+		x[r][119] = 186;
+	}
+
+	for (int c = 0; c < 120; c++)
+	{
+		x[0][c] = 205;
+		x[24][c] = 205;
+	}
+}
+
+void ladder_level1(char x[][120])
+{
+	x[23][110] = '|';
+	x[22][111] = '-';
+	x[22][112] = '-';
+	x[21][113] = '|';
+	x[20][114] = '-';
+	x[20][115] = '-';
+	x[19][116] = '|';
+	x[18][117] = '-';
+	x[18][118] = '-';
+}
+
+void hero(char x[][120], int mr, int mc)
+{
+	x[20 + mr][3 + mc] = 153;
+	x[21 + mr][3 + mc] = 186;
+	x[21 + mr][4 + mc] = '\\';
+	x[21 + mr][2 + mc] = '/';
+	x[22 + mr][3 + mc] = 186;
+	x[23 + mr][4 + mc] = '\\';
+	x[23 + mr][2 + mc] = '/';
+}
+
+void moveHero(int& mr, int& mc, char c)
+{
+	if (c == 'a' && mc > 1)
+	{
+		mc -= 3;
+		if (mc >= 105 && mr < 0)
+		{
+			mr += 2;
+		}
+	}
+
+	if (c == 'd' && mc < 114)
+	{
+		mc += 3;
+		if (mc > 105)
+		{
+			mr -= 2;
+		}
+	}
+
+	if (c == 'w')
+	{
+		isJumping = 1;
+		jumpdone = 0;
+		pos = mr;
+	}
+}
+
+void jumpHero(int& mr)
+{
+	if (isJumping == 1)
+	{
+		if (pos - velocety != mr && jumpdone == 0)
+		{
+			mr--;
+		}
+		if (pos - velocety == mr)
+		{
+			jumpdone = 1;
+		}
+		if (jumpdone == 1)
+		{
+			if (mr != 0)
+			{
+				mr++;
+			}
+		}
+		if (pos == mr && mr == 0)
+		{
+			jumpdone = 0;
+			isJumping = 0;
+		}
+	}
+}
+
+void enemy(char x[][120], int er, int ec)
+{
+	x[22 + er][3 + ec] = '[';
+	x[22 + er][4 + ec] = 'O';
+	x[22 + er][5 + ec] = ']';
+
+	x[23 + er][3 + ec] = '/';
+	x[23 + er][5 + ec] = '\\';
+}
+
+void moveEnemy(int& enec, int& enedir)
+{
+	if (enedir == 1 && enec == 60)
+		enedir = -1;
+	if (enedir == -1 && enec == 50)
+		enedir = 1;
+	enec += enedir;
+}
+
+int main()
+{
+	char x[25][120];
+	int mr = 0, mc = 0;
+	int enemyr = 0, enemyc = 55;
+	int enemydir = 1;
+	border(x);
+	ladder_level1(x);
+	hero(x, mr, mc);
+	display(x);
+	for (;;)
+	{
+		while (1)
+		{
+			while (!_kbhit())
+			{
+				border(x);
+				ladder_level1(x);
+				hero(x, mr, mc);
+				jumpHero(mr);
+				enemy(x, enemyr, enemyc);
+
+				if ((23 + mr) >= (22 + enemyr) && (20 + mr) <= (23 + enemyr) && (4 + mc) >= (3 + enemyc) && (2 + mc) <= (5 + enemyc))
+				{
+					cout << "Game Over" << endl;
+					break;
+				}
+
+				display(x);
+				moveEnemy(enemyc, enemydir);
+			}
+			char movement = _getch();
+			moveHero(mr, mc, movement);
+		}
+	}
+	return 0;
+}
