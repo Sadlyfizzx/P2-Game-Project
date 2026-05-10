5b40ec9e2e0a4079ca75a735acb543ce760820e0
Ziad Emad <sadlyfizzx@gmail.com>
2026-05-10 17:37:57 +0300

    Fixed weird motion when move in level 2 + made the HUD dynamic to move with map scrolling

--- source.cpp
+++ source.cpp
@@ -17,7 +17,7 @@ int health_hero = 100;
 int heroDir = 1;
 
 // Enemy Stats //
-int Health_enemy=100;
+int Health_enemy = 100;
 int enemyAlive = 1;
 
 // Scrolling //
@@ -25,38 +25,40 @@ const int map_width = 500;
 int camera_x = 0;
 // key //
 int key = 0;
-void display(char x[][map_width],int camera_x)
+void display(char x[][map_width], int camera_x)
 {
 	system("cls");
-	for (int r = 0; r < 70; r++)
+	for (int r = 0; r < 50; r++)
 	{
-		for (int c = camera_x; c < camera_x+120; c++)
+		for (int c = camera_x; c < camera_x + 120; c++)
 		{
 			cout << x[r][c];
 		}
 		cout << endl;
 	}
 }
-void border(char x[][map_width]) 
+void border(char x[][map_width])
 {
-	for (int r = 0; r < 70; r++)
+	for (int r = 0; r < 50; r++)
 	{
 		for (int c = 0; c < map_width; c++)
 		{
 			x[r][c] = ' ';
 		}
 	}
-	for (int r = 0; r < 70; r++)
+	for (int r = 0; r < 50; r++)
 	{
 		x[r][0] = 186;
 		x[r][119] = 186;
-		x[54][119] = ' ';
-		x[53][119] = ' ';
-		x[52][119] = ' ';
-		x[51][119] = ' ';
-		x[50][119] = ' ';
-		x[49][119] = ' ';
-		x[48][119] = ' ';
+		x[34][119] = ' ';
+		x[33][119] = ' ';
+		x[32][119] = ' ';
+		x[31][119] = ' ';
+		x[30][119] = ' ';
+		x[29][119] = ' ';
+		x[28][119] = ' ';
+		x[r][119] = 186;
+		x[r][240] = 186;
 	}
 	x[3][55] = 'L';
 	x[3][56] = 'E';
@@ -68,114 +70,113 @@ void border(char x[][map_width])
 	for (int c = 0; c < map_width; c++)
 	{
 		x[0][c] = 205;
-		x[69][c] = 205;
+		x[49][c] = 205;
 	}
 
 }
-void ladder_level1(char x[][map_width])	 
+void ladder_level1(char x[][map_width])
 {
-	x[68][105] = 179;
-	x[67][105] = 179;
-	x[66][105] = 179;
-	x[65][105] = 179;
-	x[64][105] = 179;
-	x[63][105] = 179;
-	x[62][105] = 179;
-	x[61][105] = 179;
-	x[60][105] = 179;
-	x[59][105] = 179;
-	x[58][105] = 179;
-	x[57][105] = 179;
-	x[56][105] = 179;
-	x[55][105] = 179;
-	x[68][118] = 179;
-	x[67][118] = 179;
-	x[66][118] = 179;
-	x[65][118] = 179;
-	x[64][118] = 179;
-	x[63][118] = 179;
-	x[62][118] = 179;
-	x[61][118] = 179;
-	x[60][118] = 179;
-	x[59][118] = 179;
-	x[58][118] = 179;
-	x[57][118] = 179;
-	x[56][118] = 179;
-	x[55][118] = 179;
-	x[67][106] = '-';
-	x[67][107] = '-';
-	x[67][108] = '-';
-	x[67][109] = '-';
-	x[67][110] = '-';
-	x[67][111] = '-';
-	x[67][112] = '-';
-	x[67][113] = '-';
-	x[67][114] = '-';
-	x[67][115] = '-';
-	x[67][116] = '-';
-	x[67][117] = '-';	
-	x[64][106] = '-';
-	x[64][107] = '-';
-	x[64][108] = '-';
-	x[64][109] = '-';
-	x[64][110] = '-';
-	x[64][111] = '-';
-	x[64][112] = '-';
-	x[64][113] = '-';
-	x[64][114] = '-';
-	x[64][115] = '-';
-	x[64][116] = '-';
-	x[64][117] = '-';
-	x[61][106] = '-';
-	x[61][107] = '-';
-	x[61][108] = '-';
-	x[61][109] = '-';
-	x[61][110] = '-';
-	x[61][111] = '-';
-	x[61][112] = '-';
-	x[61][113] = '-';
-	x[61][114] = '-';
-	x[61][115] = '-';
-	x[61][116] = '-';
-	x[61][117] = '-';
-	x[58][106] = '-';
-	x[58][107] = '-';
-	x[58][108] = '-';
-	x[58][109] = '-';
-	x[58][110] = '-';
-	x[58][111] = '-';
-	x[58][112] = '-';
-	x[58][113] = '-';
-	x[58][114] = '-';
-	x[58][115] = '-';
-	x[58][116] = '-';
-	x[58][117] = '-';
-	x[55][106] = '-';
-	x[55][107] = '-';
-	x[55][108] = '-';
-	x[55][109] = '-';
-	x[55][110] = '-';
-	x[55][111] = '-';
-	x[55][112] = '-';
-	x[55][113] = '-';
-	x[55][114] = '-';
-	x[55][115] = '-';
-	x[55][116] = '-';
-	x[55][117] = '-';
+	x[48][105] = 179;
+	x[47][105] = 179;
+	x[46][105] = 179;
+	x[45][105] = 179;
+	x[44][105] = 179;
+	x[43][105] = 179;
+	x[42][105] = 179;
+	x[41][105] = 179;
+	x[40][105] = 179;
+	x[39][105] = 179;
+	x[38][105] = 179;
+	x[37][105] = 179;
+	x[36][105] = 179;
+	x[35][105] = 179;
+	x[48][118] = 179;
+	x[47][118] = 179;
+	x[46][118] = 179;
+	x[45][118] = 179;
+	x[44][118] = 179;
+	x[43][118] = 179;
+	x[42][118] = 179;
+	x[41][118] = 179;
+	x[40][118] = 179;
+	x[39][118] = 179;
+	x[38][118] = 179;
+	x[37][118] = 179;
+	x[36][118] = 179;
+	x[35][118] = 179;
+	x[47][106] = '-';
+	x[47][107] = '-';
+	x[47][108] = '-';
+	x[47][109] = '-';
+	x[47][110] = '-';
+	x[47][111] = '-';
+	x[47][112] = '-';
+	x[47][113] = '-';
+	x[47][114] = '-';
+	x[47][115] = '-';
+	x[47][116] = '-';
+	x[47][117] = '-';
+	x[44][106] = '-';
+	x[44][107] = '-';
+	x[44][108] = '-';
+	x[44][109] = '-';
+	x[44][110] = '-';
+	x[44][111] = '-';
+	x[44][112] = '-';
+	x[44][113] = '-';
+	x[44][114] = '-';
+	x[44][115] = '-';
+	x[44][116] = '-';
+	x[44][117] = '-';
+	x[41][106] = '-';
+	x[41][107] = '-';
+	x[41][108] = '-';
+	x[41][109] = '-';
+	x[41][110] = '-';
+	x[41][111] = '-';
+	x[41][112] = '-';
+	x[41][113] = '-';
+	x[41][114] = '-';
+	x[41][115] = '-';
+	x[41][116] = '-';
+	x[41][117] = '-';
+	x[38][106] = '-';
+	x[38][107] = '-';
+	x[38][108] = '-';
+	x[38][109] = '-';
+	x[38][110] = '-';
+	x[38][111] = '-';
+	x[38][112] = '-';
+	x[38][113] = '-';
+	x[38][114] = '-';
+	x[38][115] = '-';
+	x[38][116] = '-';
+	x[38][117] = '-';
+	x[35][106] = '-';
+	x[35][107] = '-';
+	x[35][108] = '-';
+	x[35][109] = '-';
+	x[35][110] = '-';
+	x[35][111] = '-';
+	x[35][112] = '-';
+	x[35][113] = '-';
+	x[35][114] = '-';
+	x[35][115] = '-';
+	x[35][116] = '-';
+	x[35][117] = '-';
 }
 void hero(char x[][map_width], int mr, int mc)
 {
 	if (health_hero > 1)
 	{
-		x[66 + mr][3 + mc] = 223;
-		x[67 + mr][3 + mc] = 219;
-		x[68 + mr][4 + mc] = 221;
-		x[68 + mr][2 + mc] = 221;
+		x[46 + mr][3 + mc] = 223;
+		x[47 + mr][3 + mc] = 219;
+		x[48 + mr][4 + mc] = 221;
+		x[48 + mr][2 + mc] = 221;
 	}
 }
 void moveHero(int& mr, int& mc, char c)
 {
-	// check if on ladder
 	if (mc >= 99 && mc <= 116)
 	{
 		isClimbing = 1;
@@ -184,34 +185,34 @@ void moveHero(int& mr, int& mc, char c)
 	{
 		isClimbing = 0;
 	}
-	if (c == 'a' && mc > 1)
+	if (c == 'a' && (mc > 1 || mc > 120))
 	{
 		heroDir = -1;
 		mc -= 3;
-		if (mc >= 97)
+		if (mc >= 97 && mc <= 116)
 		{
 			mr += 2;
 			mc -= 8;
 		}
 	}
-	if (c == 'd' && mc < 97)
+	if (c == 'd' && (mc < 97 || mc>120))
 	{
 		heroDir = 1;
 		mc += 3;
-		if (mc >= 97)
+		if (mc >= 97 && mc <= 116)
 		{
 			mr -= 2;
 			mc += 8;
 		}
 	}
-	if (c == 'd' )
+	if (c == 'd')
 	{
-			mc++;
+		mc++;
 	}
 	if (c == 'w')
 	{
 		jumpLimit++;
-		if (jumpLimit <= 2 && mc < 97 && mc <= 116)
+		if (jumpLimit <= 2 && mc < 97 && mc <= 116 || mc>120)
 		{
 			isJumping = 1;
 			jumpdone = 0;
@@ -229,7 +230,7 @@ void moveHero(int& mr, int& mc, char c)
 	}
 	if (c == 's' && mc > 100)
 	{
-		mr+=3;
+		mr += 3;
 		if (mr >= 0)
 		{
 			mr -= 3;
@@ -284,116 +285,115 @@ void jumpHero(int& mr, int& mc)
 	}
 }
 
-void health_of_hero(char x[][map_width])
+void health_of_hero(char x[][map_width], int cam)
 {
 	if (health_hero == 100)
 	{
-
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][9] = 221;
-		x[3][10] = 221;
-		x[3][11] = 221;
-		x[3][12] = 221;
-		x[3][13] = 221;
-		x[3][14] = 221;
-		x[3][15] = 221;
-		x[3][16] = 221;
-		x[3][17] = 221;
-		x[3][18] = 221;
-		x[3][19] = '1';
-		x[3][20] = '0';
-		x[3][21] = '0';
-		x[3][22] = '%';
+		x[3][cam+2] = 'H';
+		x[3][cam+3] = 'E';
+		x[3][cam+4] = 'A';
+		x[3][cam+5] = 'L';
+		x[3][cam+6] = 'T';
+		x[3][cam+7] = 'H';
+		x[3][cam+8] = ':';
+		x[3][cam+9] = 221;
+		x[3][cam+10] = 221;
+		x[3][cam+11] = 221;
+		x[3][cam+12] = 221;
+		x[3][cam+13] = 221;
+		x[3][cam+14] = 221;
+		x[3][cam+15] = 221;
+		x[3][cam+16] = 221;
+		x[3][cam+17] = 221;
+		x[3][cam+18] = 221;
+		x[3][cam+19] = '1';
+		x[3][cam+20] = '0';
+		x[3][cam+21] = '0';
+		x[3][cam+22] = '%';
 	}
 	else if (health_hero == 80)
 	{
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][9] = 221;
-		x[3][10] = 221;
-		x[3][11] = 221;
-		x[3][12] = 221;
-		x[3][13] = 221;
-		x[3][14] = 221;
-		x[3][15] = 221;
-		x[3][16] = 221;
-		x[3][19] = '8';
-		x[3][20] = '0';
-		x[3][21] = '%';
+		x[3][cam+2] = 'H';
+		x[3][cam+3] = 'E';
+		x[3][cam+4] = 'A';
+		x[3][cam+5] = 'L';
+		x[3][cam+6] = 'T';
+		x[3][cam+7] = 'H';
+		x[3][cam+8] = ':';
+		x[3][cam+9] = 221;
+		x[3][cam+10] = 221;
+		x[3][cam+11] = 221;
+		x[3][cam+12] = 221;
+		x[3][cam+13] = 221;
+		x[3][cam+14] = 221;
+		x[3][cam+15] = 221;
+		x[3][cam+16] = 221;
+		x[3][cam+19] = '8';
+		x[3][cam+20] = '0';
+		x[3][cam+21] = '%';
 	}
 	else if (health_hero == 60)
 	{
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][9] = 221;
-		x[3][10] = 221;
-		x[3][11] = 221;
-		x[3][12] = 221;
-		x[3][13] = 221;
-		x[3][14] = 221;
-		x[3][19] = '6';
-		x[3][20] = '0';
-		x[3][21] = '%';
+		x[3][cam+2] = 'H';
+		x[3][cam+3] = 'E';
+		x[3][cam+4] = 'A';
+		x[3][cam+5] = 'L';
+		x[3][cam+6] = 'T';
+		x[3][cam+7] = 'H';
+		x[3][cam+8] = ':';
+		x[3][cam+9] = 221;
+		x[3][cam+10] = 221;
+		x[3][cam+11] = 221;
+		x[3][cam+12] = 221;
+		x[3][cam+13] = 221;
+		x[3][cam+14] = 221;
+		x[3][cam+19] = '6';
+		x[3][cam+20] = '0';
+		x[3][cam+21] = '%';
 	}
 	else if (health_hero == 40)
 	{
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][9] = 221;
-		x[3][10] = 221;
-		x[3][11] = 221;
-		x[3][12] = 221;
+		x[3][cam + 2] = 'H';
+		x[3][cam + 3] = 'E';
+		x[3][cam + 4] = 'A';
+		x[3][cam + 5] = 'L';
+		x[3][cam + 6] = 'T';
+		x[3][cam + 7] = 'H';
+		x[3][cam + 8] = ':';
+		x[3][cam + 9] = 221;
+		x[3][cam + 10] = 221;
+		x[3][cam + 11] = 221;
+		x[3][cam + 12] = 221;
 		x[3][19] = '4';
 		x[3][20] = '0';
 		x[3][21] = '%';
 	}
 	else if (health_hero == 20)
 	{
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][9] = 221;
-		x[3][10] = 221;
-		x[3][19] = '2';
-		x[3][20] = '0';
-		x[3][21] = '%';
+		x[3][cam + 2] = 'H';
+		x[3][cam + 3] = 'E';
+		x[3][cam + 4] = 'A';
+		x[3][cam + 5] = 'L';
+		x[3][cam + 6] = 'T';
+		x[3][cam + 7] = 'H';
+		x[3][cam + 8] = ':';
+		x[3][cam + 9] = 221;
+		x[3][cam + 10] = 221;
+		x[3][cam+19] = '2';
+		x[3][cam+20] = '0';
+		x[3][cam+21] = '%';
 	}
 	else if (health_hero == 0)
 	{
-		x[3][2] = 'H';
-		x[3][3] = 'E';
-		x[3][4] = 'A';
-		x[3][5] = 'L';
-		x[3][6] = 'T';
-		x[3][7] = 'H';
-		x[3][8] = ':';
-		x[3][19] = '0';
-		x[3][20] = '%';
+		x[3][cam + 2] = 'H';
+		x[3][cam + 3] = 'E';
+		x[3][cam + 4] = 'A';
+		x[3][cam + 5] = 'L';
+		x[3][cam + 6] = 'T';
+		x[3][cam + 7] = 'H';
+		x[3][cam + 8] = ':';
+		x[3][cam+19] = '0';
+		x[3][cam+20] = '%';
 	}
 
 }
@@ -401,7 +401,7 @@ void damage(int& mr, int& mc, int& enemyc, int& enemyr)
 {
 	if (enemyAlive == 1)
 	{
-		if ((68 + mr) >= (enemyr) && (65 + mr) <= (5 + enemyr) && (4 + mc) >= (enemyc) && (2 + mc) <= (18 + enemyc))
+		if ((48 + mr) >= (enemyr) && (45 + mr) <= (5 + enemyr) && (4 + mc) >= (enemyc) && (2 + mc) <= (18 + enemyc))
 		{
 			health_hero -= 20;
 		}
@@ -555,84 +555,84 @@ void checkgravity(int& mr)
 		mr++;
 	}
 }
-void keyhero(char x[][map_width], int& key, int& health_enemy)
+void keyhero(char x[][map_width], int& key, int& health_enemy, int cam)
 {
 	if (key == 0)
 	{
-		x[4][4] = 'K';
-		x[4][5] = 'E';
-		x[4][6] = 'Y';
-		x[4][7] = ':';
-		x[4][8] = '0';
+		x[4][cam + 4] = 'K';
+		x[4][cam + 5] = 'E';
+		x[4][cam + 6] = 'Y';
+		x[4][cam + 7] = ':';
+		x[4][cam + 8] = '0';
 	}
 	if (key == 1)
 	{
-		x[4][4] = 'K';
-		x[4][5] = 'E';
-		x[4][6] = 'Y';
-		x[4][7] = ':';
-		x[4][8] = '1';
+		x[4][cam + 4] = 'K';
+		x[4][cam + 5] = 'E';
+		x[4][cam + 6] = 'Y';
+		x[4][cam + 7] = ':';
+		x[4][cam + 8] = '1';
 	}
 	if (key == 2)
 	{
-		x[4][4] = 'K';
-		x[4][5] = 'E';
-		x[4][6] = 'Y';
-		x[4][7] = ':';
-		x[4][8] = '2';
+		x[4][cam + 4] = 'K';
+		x[4][cam + 5] = 'E';
+		x[4][cam + 6] = 'Y';
+		x[4][cam + 7] = ':';
+		x[4][cam + 8] = '2';
 	}
 	if (key == 3)
 	{
-		x[4][4] = 'K';
-		x[4][5] = 'E';
-		x[4][6] = 'Y';
-		x[4][7] = ':';
-		x[4][8] = '3';
-	}
-	if (health_enemy==0)
-	{
-		key += 1;
-	}
-}
-void health_enemy(char x[][map_width])
-{
-	if (Health_enemy == 100)
-	{
-		x[3][90] = 'E';
-		x[3][91] = 'n';
-		x[3][92] = 'e';
-		x[3][93] = 'm';
-		x[3][94] = 'y';
-		x[3][95] = ':';
-		x[3][96] = 221;
-		x[3][97] = 221;
-		x[3][98] = 221;
-		x[3][99] = 221;
-		x[3][100] = 221;
-		x[3][101] = 221;
-		x[3][102] = 221;
-		x[3][103] = 221;
-		x[3][104] = 221;
-		x[3][105] = '1';
-		x[3][106] = '0';
-		x[3][107] = '0';
-		x[3][108] = '%';
+		x[4][cam + 4] = 'K';
+		x[4][cam + 5] = 'E';
+		x[4][cam + 6] = 'Y';
+		x[4][cam + 7] = ':';
+		x[4][cam + 8] = '3';
 	}
 	if (Health_enemy == 0)
 	{
-		x[3][90] = 'E';
-		x[3][91] = 'n';
-		x[3][92] = 'e';
-		x[3][93] = 'm';
-		x[3][94] = 'y';
-		x[3][95] = ':';
-		x[3][107] = '0';
-		x[3][108] = '%';
+		key = 1;
+	}
+}
+void health_enemy(char x[][map_width], int cam)
+{
+	if (Health_enemy == 100)
+	{
+		x[3][cam + 90] = 'E';
+		x[3][cam + 91] = 'n';
+		x[3][cam + 92] = 'e';
+		x[3][cam + 93] = 'm';
+		x[3][cam + 94] = 'y';
+		x[3][cam + 95] = ':';
+		x[3][cam + 96] = 221;
+		x[3][cam + 97] = 221;
+		x[3][cam + 98] = 221;
+		x[3][cam + 99] = 221;
+		x[3][cam + 100] = 221;
+		x[3][cam + 101] = 221;
+		x[3][cam + 102] = 221;
+		x[3][cam + 103] = 221;
+		x[3][cam + 104] = 221;
+		x[3][cam + 105] = '1';
+		x[3][cam + 106] = '0';
+		x[3][cam + 107] = '0';
+		x[3][cam + 108] = '%';
+	}
+	if (Health_enemy == 0)
+	{
+		x[3][cam + 90] = 'E';
+		x[3][cam + 91] = 'n';
+		x[3][cam + 92] = 'e';
+		x[3][cam + 93] = 'm';
+		x[3][cam + 94] = 'y';
+		x[3][cam + 95] = ':';
+		x[3][cam + 107] = '0';
+		x[3][cam + 108] = '%';
 	}
 }
 void killEnemy(int& mr, int& mc, int enemyc, int enemyr)
 {
-	int heroPos = 68 + mr;
+	int heroPos = 48 + mr;
 	int sameCol = 0; // flag to check if hero at the same column as enemy
 
 	if (4 + mc >= enemyc && 2 + mc <= enemyc + 18)
@@ -663,7 +663,7 @@ void killEnemy(int& mr, int& mc, int enemyc, int enemyr)
 	}
 }
 
-void resetGame(char x[][map_width], int& mr, int& mc, int& enemyr, int& enemyc, int& enemydir)
+void resetGame(char x[][map_width], int& camera_x, int& mr, int& mc, int& enemyr, int& enemyc, int& enemydir)
 {
 	system("cls");
 	cout << "Game Over. Press 'R' to restart or 'Q' to quit." << endl;
@@ -697,60 +697,59 @@ void resetGame(char x[][map_width], int& mr, int& mc, int& enemyr, int& enemyc, 
 	// Reset positions
 	mr = 0;
 	mc = 0;
-	enemyr = 63;
+	enemyr = 43;
 	enemyc = 55;
 	enemydir = 1;
 
 	// Rebuild map
 	border(x);
 	ladder_level1(x);
-	health_of_hero(x);
-	health_enemy(x);
+	health_of_hero(x, camera_x);
+	health_enemy(x, camera_x);
 }
-void checkLose(char x[][map_width], int& mr, int& mc, int& enemyr, int& enemyc, int& enemydir)
+void checkLose(char x[][map_width], int& camera_x, int& mr, int& mc, int& enemyr, int& enemyc, int& enemydir)
 {
 	if (health_hero <= 0)
 	{
-		resetGame(x, mr, mc, enemyr, enemyc, enemydir);
+		resetGame(x, camera_x, mr, mc, enemyr, enemyc, enemydir);
 	}
 }
 
 int main()
 {
-	char x[70][map_width];
+	char x[50][map_width];
 	int mr = 0, mc = 0;
-	int enemyr = 63, enemyc = 55; // enemy row and column
+	int enemyr = 43, enemyc = 55; // enemy row and column
 	int enemydir = 1; // enemy direction (1 for right, -1 for left)
 	border(x);
 	ladder_level1(x);
 	hero(x, mr, mc);
-	health_of_hero(x);
-	display(x,camera_x);
-		while (1)
-		{ 
-			while (!_kbhit())
-			{
-				camera_x = mc - 60;
-				if (camera_x < 0) camera_x = 0;
-				if (camera_x > map_width - 120) camera_x = map_width - 120;
-				border(x);
-				keyhero(x,key, Health_enemy);
-				ladder_level1(x);
-				hero(x, mr, mc);
-				jumpHero(mr, mc);
-				enemy(x, enemyr, enemyc, enemydir);
-				damage(mr, mc, enemyc, enemyr);
-				killEnemy(mr, mc, enemyc, enemyr);
-				checkLose(x,mr,mc,enemyr,enemyc,enemydir);
-				health_of_hero(x);
-				health_enemy(x);
-				checkgravity(mr);
-				display(x,camera_x);
-				moveEnemy(enemyc, enemydir);
-				Sleep(100);
-			}
-			char movement = _getch();
-			moveHero(mr, mc, movement);
+	health_of_hero(x, camera_x);
+	display(x, camera_x);
+	while (1)
+	{
+		while (!_kbhit())
+		{
+			camera_x = mc - 90;
+			if (camera_x < 0) camera_x = 0;
+			if (camera_x > map_width - 120) camera_x = map_width - 120;
+			border(x);
+			keyhero(x, key, Health_enemy, camera_x);
+			ladder_level1(x);
+			hero(x, mr, mc);
+			jumpHero(mr, mc);
+			enemy(x, enemyr, enemyc, enemydir);
+			damage(mr, mc, enemyc, enemyr);
+			killEnemy(mr, mc, enemyc, enemyr);
+			checkLose(x, camera_x, mr, mc, enemyr, enemyc, enemydir);
+			health_of_hero(x, camera_x);
+			health_enemy(x, camera_x);
+			checkgravity(mr);
+			display(x, camera_x);
+			moveEnemy(enemyc, enemydir);
 		}
+		char movement = _getch();
+		moveHero(mr, mc, movement);
+	}
 	return 0;
 }
